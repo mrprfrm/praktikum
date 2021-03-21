bb66a46a1277f4cba866e094ca71bb510e7d4a50
Anton Petrov <marblemonolit@gmail.com>
2021-03-21 22:11:56 +0300

    resolve #5 admin panel initialization
    admin service created;
    fts service dependencies fixed;
    fts index configuration updated.

---
+++ admin/schema/create_schema.sql
@@ -0,0 +1,39 @@
+DROP SCHEMA IF EXISTS content CASCADE;
+
+DROP ROLE IF EXISTS content;
+
+CREATE ROLE content WITH LOGIN PASSWORD 'content';
+
+CREATE SCHEMA AUTHORIZATION content;
+
+CREATE TABLE content.persons(
+    id UUID NOT NULL,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE content.genres(
+    id UUID NOT NULL,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE content.movies(
+    id UUID NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    plot TEXT,
+    imdb_rating DECIMAL DEFAULT 0
+);
+
+CREATE TYPE person_position AS ENUM ('director', 'writer', 'actor');
+
+CREATE TABLE content.movies_person(
+    id UUID NOT NULL,
+    movie_id UUID NOT NULL,
+    person_id UUID NOT NULL,
+    position person_position
+);
+
+CREATE TABLE content.movies_genres(
+    id UUID NOT NULL,
+    movie_id UUID NOT NULL,
+    genre_id UUID NOT NULL
+);
--- fabfile.py
+++ fabfile.py
@@ -10,7 +10,7 @@ def run(service_name):
     except:
         pass
     finally:
-        local(f'docker-compose run --name {PROJECT_NAME}_{service_name} --service-ports --use-aliases fulltextsearch poetry run flask run --host=0.0.0.0 --port=8000')
+        local(f'docker-compose run --name {PROJECT_NAME}_{service_name} --service-ports --use-aliases {service_name} poetry run flask run --host=0.0.0.0 --port=8000')
 
 
 def bash(service_name):
@@ -24,8 +24,18 @@ def shell(service_name):
 def etl(path):
     local(f'docker exec -it {PROJECT_NAME}_fulltextsearch poetry run flask etl {path}')
 
+
 def init_index(path=''):
     if path:
         local(f'docker exec -it {PROJECT_NAME}_fulltextsearch poetry run flask init-index --c {path}')
     else:
         local(f'docker exec -it {PROJECT_NAME}_fulltextsearch poetry run flask init-index')
+
+
+def init_admin_shema():
+    local(f'cat admin/schema/create_schema.sql | docker exec -i {PROJECT_NAME}_database psql postgres -U postgres')
+    local(f'cat admin/schema/create_indexes.sql | docker exec -i {PROJECT_NAME}_database psql postgres -U postgres')
+
+
+def rundb():
+    local(f'docker run --rm -p 5432:5432 --name {PROJECT_NAME}_testdb -e POSTGRES_PASSWORD=postgres postgres:13')
